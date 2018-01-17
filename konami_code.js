63803ebd30ae086994eb0c034eb202cb8a95f262
Dan Brett <boristhellama99@gmail.com>
2018-01-17 16:56:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 var index = 0;
 
-function init(e) {
-  const key = e.detail;
+function init(event) {
+  const key = event.detail;
   if (key === code[i]) {
     index++;
   }
@@ -19,4 +19,4 @@ function init(e) {
 }
 
 
-document.addEventListener('keydown', init);
+document.body.addEventListener('keydown', init());
