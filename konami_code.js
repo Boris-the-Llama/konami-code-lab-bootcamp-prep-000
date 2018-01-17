3f0ac9056b35641f09a778720b7ff6d78df61271
Dan Brett <boristhellama99@gmail.com>
2018-01-17 17:24:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,24 +3,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var codepos = 0;
 
 function init() {
-  let index = 0;
- 
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  
-  if (key === alphabet[index]) {
-    index++;
+  if (key === code[index]) {
+    codepos++;
  
-    if (index === alphabet.length) {
+    if (codepos === code.length) {
       alert("Hurray!");
  
-      index = 0;
+      codepos = 0;
     }
   } else {
-    index = 0;
+    codepos = 0;
   }
-}
+
 }
 
 
