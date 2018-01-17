a212ce786f56f5d0ccfd29068cec03e0fc226aee
Dan Brett <boristhellama99@gmail.com>
2018-01-17 17:28:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var codepos = 0;
 
 function init() {
-  document.addEventListener('keydown', init);
+  document.body.addEventListener('keydown', init);
   const key = parseInt(this.detail || this.which);
  
   if (key === code[codepos]) {
