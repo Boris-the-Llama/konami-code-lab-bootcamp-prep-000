8573197910b24281038549a62332e6a3bf7cfe88
Dan Brett <boristhellama99@gmail.com>
2018-01-17 17:23:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,22 +3,25 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var codepos = 0;
 
 function init() {
-  var key = code[e.KeyCode];
-  
-  
-  if (key === code[index]) {
+  let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
     index++;
  
-    if (index === code.length) {
+    if (index === alphabet.length) {
       alert("Hurray!");
  
       index = 0;
     }
-  } else { 
+  } else {
     index = 0;
   }
-  
+}
 }
 
 
-document.addEventListener('keydown', init;
+document.addEventListener('keydown', init);
