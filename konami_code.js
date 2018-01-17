b70c4def8f45cad0fa6a65974b0c4847343fcf46
Dan Brett <boristhellama99@gmail.com>
2018-01-17 16:57:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 var index = 0;
 
-function init(event) {
-  const key = event.detail;
+function init() {
+  const key = (e.detail || e.which);
   if (key === code[i]) {
     index++;
   }
